4570814550fb06ba4032d5418668dd5bc5818e94
aapohu <aapo.huhtalo@gmail.com>
2021-10-08 01:50:54 +0300

    Change TEXT to VARCHAR

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, 
-            username TEXT UNIQUE (30), password TEXT (200), 
+            username VARCHAR UNIQUE (30), password VARCHAR (200), 
             joindate TIMESTAMP, visible BOOLEAN);
 
 CREATE TABLE stations (id SERIAL PRIMARY KEY, 
-            station_name TEXT (100), addr TEXT (150), city TEXT (100), 
-            postnr TEXT (10), road TEXT (10), operational BOOLEAN, 
+            station_name VARCHAR (100), addr VARCHAR (150), city VARCHAR (100), 
+            postnr VARCHAR (10), road VARCHAR (10), operational BOOLEAN, 
             visible BOOLEAN);
 
 CREATE TABLE prices (id SERIAL PRIMARY KEY, station_id INTEGER REFERENCES stations,
@@ -12,14 +12,14 @@ CREATE TABLE prices (id SERIAL PRIMARY KEY, station_id INTEGER REFERENCES statio
             type1_price FLOAT(5), type2_price FLOAT(5), type3_price FLOAT(5), type4_price FLOAT (5));
 
 CREATE TABLE requests (id SERIAL PRIMARY KEY, sender_id INTEGER REFERENCES users,
-            message TEXT (200), time TIMESTAMP, visible BOOLEAN);
+            message VARCHAR (200), time TIMESTAMP, visible BOOLEAN);
 
 CREATE TABLE chat (id SERIAL PRIMARY KEY, sender_id INTEGER REFERENCES users,
-            message TEXT, time TIMESTAMP);
+            message VARCHAR, time TIMESTAMP);
 
 CREATE TABLE admin_users (user_id INTEGER REFERENCES users);
 
-CREATE TABLE products (id SERIAL PRIMARY KEY, name TEXT (30));
+CREATE TABLE products (id SERIAL PRIMARY KEY, name VARCHAR (30));
 
 INSERT INTO users (username,password,joindate) VALUES ('BOSS','pbkdf2:sha256:150000$V0pONHil$d8abb05ccc68d571eee30bffe99f6be7515126e90c4580f2f6912f3e8634dc67',NOW());
 
